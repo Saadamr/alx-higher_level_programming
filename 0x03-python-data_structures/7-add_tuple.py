10357171c70d47a8de73a864aeff7072ad9f0f9d
Saadamr <s.amroui@mundiapolis.ma>
2024-01-08 16:29:27 -0800

    ez

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,4 @@
-
-i#!/usr/bin/python3
+#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     list_a = list(tuple_a)
     list_b = list(tuple_b)
@@ -14,3 +13,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     list_c = [list_a[0] + list_b[0], list_a[1] + list_b[1]]
     return (tuple(list_c))
+
